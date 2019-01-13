dbc7fecd7ac2b9324a18c698b63a3ec771b5d4f1
nargok <nargok@gmail.com>
2019-01-13 17:24:40 +0900

    implement get single person

--- main.go
+++ main.go
@@ -24,14 +24,14 @@ type Address struct {
 var people []Person
 
 func main() {
-	// people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "1", Firstname: "Taro", Lastname: "Yamada", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Jiro", Lastname: "Koyama", Address: &Address{City: "City Y", State: "State Y"}})
 	people = append(people, Person{ID: "3", Firstname: "Sabro", Lastname: "Tanaka"})
+	people = append(people, Person{ID: "4", Firstname: "kisabro", Lastname: "tsuruoka", Address: &Address{City: "City V"}})
 
 	router := mux.NewRouter()
 	router.HandleFunc("/people", GetPeople).Methods("GET")
-	// router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 	// router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
 	// router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 	fmt.Println("The server is runnning on http://localhost:8000")
@@ -40,4 +40,16 @@ func main() {
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	// 指定したIDのPersonが無かったときに{}を返す
+	json.NewEncoder(w).Encode(&Person{})
 }

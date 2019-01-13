d130cf9a28a9b85fc4ef5175f5ef6f23d256ee1c
nargok <nargok@gmail.com>
2019-01-13 16:38:27 +0900

    setup router

--- main.go
+++ main.go
@@ -9,5 +9,9 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Method("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Method("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Method("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Method("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
